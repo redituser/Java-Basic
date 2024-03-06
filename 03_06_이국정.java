19a6ac91255f26abdf682ced866a911e65d6a28f
redituser <162386111+redituser@users.noreply.github.com>
2024-03-06 17:50:49 +0900

    Add files via upload

---
+++ 03_06_이국정.java
@@ -0,0 +1,18 @@
+package sec06;
+
+public class Hello {
+	public static void main(String[] args) {
+		String name = "홍길동";
+		int age = 20;
+		
+		System.out.println("이름은 :" + name);
+		System.out.println("나이 :" + age);
+		System.out.println();
+//		System.out.println("이름은 :" + name);
+//		System.out.println("나이 :" + age);
+		
+		System.out.println("이름 :" + name + " 나이: " + age);
+		
+	}
+
+}
