b038b959f6c405d5c63bfe563a7886ad4aabf030
redituser <162386111+redituser@users.noreply.github.com>
2024-03-11 15:45:31 +0900

    Add files via upload

---
+++ Typeconversion03.java
@@ -0,0 +1,18 @@
+package TypeConversion03;
+import java.util.Scanner;
+
+public class Typeconversion03 {
+
+	public static void main(String[] args) {
+		int x = 10;
+		double y = 3.0;
+		
+		System.out.println(x/y);
+		
+		System.out.println((int)(x/y));
+		
+	
+		
+		
+	}
+}
