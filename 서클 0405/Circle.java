74deae496f6ef796424700b55daca71aaa21f092
redituser <162386111+redituser@users.noreply.github.com>
2024-04-06 17:16:50 +0900

    Add files via upload

---
+++ 서클 0405/Circle.java
@@ -0,0 +1,14 @@
+package chap10;
+
+class Circle {
+	double radius;
+	final double PI = 3.14;
+
+	double getArea() {
+		return PI * radius * radius;
+	}
+
+	void show(double radius, double area) {
+		System.out.printf("반지름 = %.2f, 면적 = %.2f\n", radius, area);
+	}
+}
